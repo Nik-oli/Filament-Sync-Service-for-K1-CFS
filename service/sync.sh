d43a58178050136a62347228bdc9b7a8415cd19e
Shane Lupton <shanelupton@hurricaneprint.com>
2025-04-25 14:06:04 -0600

    Fixed directory

--- service/sync.sh
+++ service/sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-syncdirectory="${PWD}/Filament-Sync-Service/data"
+syncdirectory="$HOME/Filament-Sync-Service/data"
 crealitydirectory="/mnt/UDISK/creality/userdata/box"
 
 cp -a $syncdirectory/* $crealitydirectory
