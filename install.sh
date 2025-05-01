18eded385d699f8ed1eefbdaaed718cfc4d03bcf
Shane Lupton <shanelupton@hurricaneprint.com>
2025-04-30 21:21:59 -0600

    Service is now syncing at boot

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ echo "Creating data directory"
 mkdir -p data
 
 #install and enable startup service
+echo "Installing and enabling service"
 cp ${servicedirectory}/filamentsync /etc/init.d/
 chmod +x /etc/init.d/filamentsync
 chmod +x ${servicedirectory}/sync.sh
@@ -14,6 +15,5 @@ chmod +x ${servicedirectory}/sync.sh
 /etc/init.d/filamentsync start
 
 echo "Ready to sync"
-echo "Make sure tool is set in your slicers post-processing options"
+echo "Make sure the script is set in your slicers post-processing options"
 echo "or in terminal run node main.js in the filament-sync directory"
-echo "Check the github repo if you need help"
