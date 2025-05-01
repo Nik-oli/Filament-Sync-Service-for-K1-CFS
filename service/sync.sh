5b52a93264e56c5148ecb32350e0b7079ec05dbf
Shane Lupton <shanelupton@hurricaneprint.com>
2025-05-01 01:45:23 -0600

    Changed formatting

--- service/sync.sh
+++ service/sync.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 if test -f "${HOME}/Filament-Sync-Service/service/sync.sh"; then
-SYNCDIRECTORY="${HOME}/Filament-Sync-Service/data"
+    SYNCDIRECTORY="${HOME}/Filament-Sync-Service/data"
 else
-SYNCDIRECTORY="${HOME}/Filament-Sync-Service/data" 
+    SYNCDIRECTORY="${HOME}/Filament-Sync-Service/data" 
 fi
 
 CREALITYDIRECTORY="/mnt/UDISK/creality/userdata/box"
 
 while :
 do
-    if test -f "${HOME}/Filament-Sync-Service/data/material_database.json"; then
+    if test -f "$SYNCDIRECTORY/material_database.json"; then
         syncedfilesize=$(stat -c%s "$SYNCDIRECTORY/material_database.json")
         targetfilesize=$(stat -c%s "$CREALITYDIRECTORY/material_database.json")
         if([ $syncedfilesize != $targetfilesize ]);then
-            cp -a ${SYNCDIRECTORY}/* ${CREALITYDIRECTORY}
+            cp -a ${SYNCDIRECTORY}/. ${CREALITYDIRECTORY}
         fi
     fi
 
