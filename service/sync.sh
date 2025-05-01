a3b5e25d5e2eec310f474c6117ed49d4807798a6
Shane Lupton <shanelupton@hurricaneprint.com>
2025-05-01 01:30:40 -0600

    Don't sync if new database hasn't been generated

--- service/sync.sh
+++ service/sync.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
-if test -f "/root/Filament-Sync-Service/service/sync.sh"; then
-SYNCDIRECTORY="/root/Filament-Sync-Service/data"
+if test -f "${HOME}/Filament-Sync-Service/service/sync.sh"; then
+SYNCDIRECTORY="${HOME}/Filament-Sync-Service/data"
 else
-SYNCDIRECTORY="/mnt/UDISK/root/Filament-Sync-Service/data" 
+SYNCDIRECTORY="${HOME}/Filament-Sync-Service/data" 
 fi
 
 CREALITYDIRECTORY="/mnt/UDISK/creality/userdata/box"
 
 while :
 do
-    syncedfilesize=$(stat -c%s "$SYNCDIRECTORY/material_database.json")
-    targetfilesize=$(stat -c%s "$CREALITYDIRECTORY/material_database.json")
-    if([ $syncedfilesize != $targetfilesize ]);then
-        cp -a ${SYNCDIRECTORY}/* ${CREALITYDIRECTORY}
+    if test -f "${HOME}/Filament-Sync-Service/data/material_database.json"; then
+        syncedfilesize=$(stat -c%s "$SYNCDIRECTORY/material_database.json")
+        targetfilesize=$(stat -c%s "$CREALITYDIRECTORY/material_database.json")
+        if([ $syncedfilesize != $targetfilesize ]);then
+            cp -a ${SYNCDIRECTORY}/* ${CREALITYDIRECTORY}
+        fi
     fi
+
     sleep 15
 done
